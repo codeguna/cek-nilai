a14d55b88479c2f6c32e959eca5f38ecdf84dcc7
codeguna <pratamagunadhi@gmail.com>
2023-01-17 12:57:49 +0700

    set h4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4><i class="fa fa-search" aria-hidden="true"></i> List Nama Mahasiswa</h4>
+                        <h1 class="display-4"><i class="fa fa-search" aria-hidden="true"></i> List Nama Mahasiswa</h1>
                     </div>
 
                     <div class="card-body">
